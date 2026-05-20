add528861e1a10b9f09f9a5200deff9c68d5db5e
SettingDust <settingdust@gmail.com>
2026-05-20 13:58:59 +0800

    feat: supporting any mods providing the kotlin

--- src/common/common/main/java/settingdust/klf_x_kff/KLFxKFFTransformationService.java
+++ src/common/common/main/java/settingdust/klf_x_kff/KLFxKFFTransformationService.java
@@ -7,7 +7,6 @@ import cpw.mods.modlauncher.api.IEnvironment;
 import cpw.mods.modlauncher.api.ITransformationService;
 import cpw.mods.modlauncher.api.ITransformer;
 import cpw.mods.modlauncher.api.IncompatibleEnvironmentException;
-import net.minecraftforge.fml.loading.moddiscovery.ModFile;
 import org.jetbrains.annotations.NotNull;
 import settingdust.klf_x_kff.accessor.JarAccessor;
 import settingdust.klf_x_kff.accessor.SimpleJarMetadataAccessor;
@@ -22,35 +21,36 @@ public class KLFxKFFTransformationService implements ITransformationService {
     public KLFxKFFTransformationService() {
         PreloadingTricksCallbacks.SETUP_MODS.register(_manager -> {
             var manager = (LexForgeModManager) _manager;
-            ModFile kffFile = null;
-            ModFile klfFile = null;
-            for (final var modFile : manager.all()) {
-                if (modFile.getSecureJar().name().equals("thedarkcolour.kotlinforforge")) {
-                    kffFile = modFile;
-                }
-                if (modFile.getSecureJar().name().equals("klf")) {
-                    klfFile = modFile;
-                }
-            }
-            if (kffFile == null || klfFile == null) return;
+            var klfFile = manager.all()
+                    .stream()
+                    .filter(it -> it.getSecureJar().name().equals("klf"))
+                    .findAny()
+                    .orElse(null);
             var klfPackages = klfFile.getSecureJar().getPackages();
             var klfProvides =
-                klfFile.getSecureJar().getProviders().stream().map(SecureJar.Provider::serviceName).collect(Collectors.toSet());
-            var kffJar = (Jar) kffFile.getSecureJar();
+                    klfFile.getSecureJar()
+                            .getProviders()
+                            .stream()
+                            .map(SecureJar.Provider::serviceName)
+                            .collect(Collectors.toSet());
 
-            var metadata = (SimpleJarMetadata) JarAccessor.getMetadata(kffJar);
-            SimpleJarMetadataAccessor.setPkgs(
-                metadata, kffJar.getPackages().stream()
+            for (var modFile : manager.all()) {
+                if (modFile == klfFile) continue;
+                var jar = (Jar) modFile.getSecureJar();
+                var metadata = (SimpleJarMetadata) JarAccessor.getMetadata(jar);
+                SimpleJarMetadataAccessor.setPkgs(
+                        metadata, jar.getPackages().stream()
                                 .filter(it -> !klfPackages.contains(it))
                                 .collect(Collectors.toSet())
-            );
-            SimpleJarMetadataAccessor.setProviders(
-                metadata,
-                metadata.providers()
-                        .stream()
-                        .filter(it -> !klfProvides.contains(it.serviceName()))
-                        .toList()
-            );
+                );
+                SimpleJarMetadataAccessor.setProviders(
+                        metadata,
+                        metadata.providers()
+                                .stream()
+                                .filter(it -> !klfProvides.contains(it.serviceName()))
+                                .toList()
+                );
+            }
         });
     }
 
